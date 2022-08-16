1f4da424f727d0cf120f0c5d8ba0e232b0c83ad9
Chibu <atuchibueze@gmail.com>
2022-08-16 13:36:06 -0400

    implement outputHtml function to display results on webpage

--- js/main.js
+++ js/main.js
@@ -8,15 +8,34 @@ async function searchStates(searchText) {
 
   // get matches to current input text
   let matches = states.filter((state) => {
-    const regex = new RegExp(`^${searchText}`, "gi");
+    const regex = new RegExp(`^${searchText}`, `gi`);
     return state.name.match(regex) || state.abbr.match(regex);
   });
 
   if (!searchText) {
     matches = [];
+    matchList.innerHTML = "";
   }
 
-  console.log(matches);
+  outputHtml(matches);
+}
+
+// show results in HTML
+function outputHtml(matches) {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+      <div class="card card-body mb-1">
+        <h4>${match.name} (${match.abbr}) <span class="text-primary">${match.capital}</span></h4>
+        <small>Lat: ${match.lat} / Long: ${match.long}</small>
+      </div>
+    `
+      )
+      .join("");
+
+    matchList.innerHTML = html;
+  }
 }
 
 search.addEventListener("input", () => searchStates(search.value));
